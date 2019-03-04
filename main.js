19df117b7074291c64ff1a1b30f580eb1f54e810
Vendrieou <33180723+Vendrieou@users.noreply.github.com>
2019-03-04 07:24:21 +0700

    Add files via upload
    Final Project
    Hactiv 8

---
+++ main.js
@@ -0,0 +1,13 @@
+$('h1').css({"bacground-color":"green"});
+
+$('.d-block,w-100').css({"width":"330px","height":"500px"});
+
+$('address').css({
+    "background-color":"#720912 ",
+    "text-align":"left",
+    "color":"white"
+});
+$('#at').css({
+    "font-family":"sans-serif",
+    "font-size":"20px"
+});
